d40f91aac4a1468f1889ac7c71c965f14124ca52
Bruno Mariz <bruno.mariz@semantix.inc>
2022-02-16 14:18:21 -0300

    :recycle: add hooks and :sparkles: add search field card

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,50 +2,48 @@ import "./App.css";
 import { users } from "../data/users";
 import CardList from "../components/CardList";
 import "tachyons";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchField from "../components/SearchField";
 import ScrollBox from "../components/ScrollBox";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      search_field: "",
-      users: users,
-    };
-  }
+const App = () => {
+  // Define states
+  const [searchField, setSearchField] = useState("");
+  const [usersState, setUsersState] = useState(users);
 
-  on_change = (event) => {
-    this.setState({ search_field: event.target.value });
+  const on_change = (event) => {
+    setSearchField(event.target.value);
   };
 
-  get_user() {
-    const filtered_users = this.state.users.filter((item) =>
-      item.username
-        .toLowerCase()
-        .includes(this.state.search_field.toLowerCase())
+  const get_user = () => {
+    // Filter users whose username includes the searchfield value
+    let filtered_users = usersState.filter((item) =>
+      item.username.toLowerCase().includes(searchField.toLowerCase())
     );
 
-    if (filtered_users.length == 0) {
-      console.log(this.state.search_field);
-      return [{ username: this.state.search_field }];
+    //  Return the searchfield if no users include the searchfield
+    // if (filtered_users.length == 0) {
+    //   console.log(searchField);
+    //   return [{ username: searchField }];
+    // }
+
+    // Add new user with username equal to the searchfield to the filtered users array
+    if (searchField.length > 0) {
+      filtered_users.unshift({ username: searchField });
     }
 
     return filtered_users;
-  }
+  };
 
-  render() {
-    return (
-      <div className='tc'>
-        <h1 className=''>CATFRIENDS</h1>
-        <SearchField on_change={this.on_change}></SearchField>
-
-        <ScrollBox>
-          <CardList users={this.get_user()}></CardList>
-        </ScrollBox>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='tc'>
+      <h1 className=''>CATFRIENDS</h1>
+      <SearchField on_change={on_change}></SearchField>
+      <ScrollBox>
+        <CardList users={get_user()}></CardList>
+      </ScrollBox>
+    </div>
+  );
+};
 
 export default App;
